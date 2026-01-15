00d132aa6102940930d067bccfbe02208e1d67c3
sadda <lukas.adam.cr@gmail.com>
2026-01-15 16:15:38 +0100

    Added next_prev

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import numpy as np
 import pandas as pd
 from PIL import Image, ImageTk
 import os
@@ -8,6 +9,9 @@ from utils import get_index
 
 DATA_CSV = "data.csv"
 ANS_CSV = "answers.csv"
+ANS_POS = "same"
+ANS_NEG = "diff"
+ANS_UNK = "unknown"
 ROOT = "data/TurtlesOfSMSRC"
 
 class App:
@@ -21,11 +25,13 @@ class App:
         assert isinstance(self.df.index, pd.RangeIndex)
         
         self.idx = 0
+        self.increase = True
 
         if os.path.exists(ANS_CSV):
-            self.answers = pd.read_csv(ANS_CSV, index_col=0)
+            self.answers = pd.read_csv(ANS_CSV)
         else:
-            self.answers = pd.DataFrame(columns=["answer"])
+            self.answers = self.df.copy()
+            self.answers['answer'] = None
 
         self._build_ui()
         self._bind_keys()
@@ -65,13 +71,13 @@ class App:
             self.root.bind(key, lambda e, a=action: a())
 
     def on_same(self, event=None):
-        self.answer("same")
+        self.answer(ANS_POS)
 
     def on_diff(self, event=None):
-        self.answer("different")
+        self.answer(ANS_NEG)
 
     def on_unknown(self, event=None):
-        self.answer("unknown")
+        self.answer(ANS_UNK)
 
     def on_heads(self, event=None):
         self.show_heads()
@@ -92,6 +98,19 @@ class App:
                 raise ValueError('Identity is different. Segmentation.csv is probably wrong')
         return self.dataset[j]
 
+    def _answer_exists(self, identity1, identity2):
+        idx1 = (self.df['identity1'] == identity1) * (self.df['identity2'] == identity2)
+        idx2 = (self.df['identity1'] == identity2) * (self.df['identity2'] == identity1)
+        idx = idx1 + idx2
+        ans = self.answers.loc[idx, 'answer']
+        return (ans.isin([ANS_NEG, ANS_POS])).any()
+
+    def next_prev(self):
+        if self.increase:
+            self.next()
+        else:
+            self.prev()
+
     def load_images(self, row):
         img1 = self._load_image(row['image_id1'], identity=row['identity1'])
         img2 = self._load_image(row['image_id2'], identity=row['identity2'])
@@ -102,10 +121,15 @@ class App:
         self.canvas_r.delete("all")
 
         row = self.df.iloc[self.idx]
+
+        if self._answer_exists(row['identity1'], row['identity2']):
+            self.next_prev()
+            return
+        
         img1, img2 = self.load_images(row)
 
         if img1 is None or img2 is None:
-            self.next()
+            self.next_prev()
             return
 
         self.tk_img1 = ImageTk.PhotoImage(img1)
@@ -116,7 +140,7 @@ class App:
 
     def answer(self, answer):
         self.answers.loc[self.idx, "answer"] = answer
-        self.answers.to_csv(ANS_CSV)
+        self.answers.to_csv(ANS_CSV, index=False)
         self.next()
 
     def show_heads(self):
@@ -124,6 +148,7 @@ class App:
         pass
 
     def next(self):
+        self.increase = True
         if self.idx < len(self.df) - 1:
             self.idx += 1
             self.load_row()
@@ -131,9 +156,12 @@ class App:
             self.root.destroy()
 
     def prev(self):
+        self.increase = False
         if self.idx > 0:
             self.idx -= 1
             self.load_row()
+        else:
+            self.root.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
